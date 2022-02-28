b2fbf9fee43a0e5f5dc4f075caa863ae5a0bd32e
arya56 <arya56@yahoo.com>
2022-02-28 10:35:02 +0100

    homework

---
+++ week1/Homework/array_task5/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// int[] result = AddArray(splitArray);
+// WriteResult(result);
+
+int[] input = new[] { 1, 2, 5, 7, 2, 3, 5, 7 };
+int[][] splitArray = Split(input);
+
+int[][] Split(int[] inputArray)
+{
+    int[] secondArray = { };
+    int[][] twoDimentionArray = new int[2][];
+    if (inputArray.Length % 2 == 0)
+    {
+        for (var i = 0; i < inputArray.Length / 2; i++)
+        {
+            Console.WriteLine(inputArray[i]);
+            twoDimentionArray[0][i] = inputArray[i];
+        }
+        for (var i = inputArray.Length; i < inputArray.Length; i++)
+        {
+            twoDimentionArray[1][i] = inputArray[i];
+        }
+        //firstArray = inputArray.Take(4).ToArray();
+        // secondArray=inputArray.Skip(inputArray.Length/2);
+        // return firstArray,secondArray;
+    }
+    else
+    {
+        Console.WriteLine("Array lenght is not even");
+    }
+    return twoDimentionArray;
+}
---
+++ week1/Homework/math_array/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = new[] { 271, -3, 1, 14, -100, 13, 2, 1, -8, -59, -1852, 41, 5 };
+int[] result = GetResult(arr); //TODO: Implement GetResult
+Console.WriteLine($"Sum of negative numbers: {result[0]}. Multiplication of positive numbers: {result[1]}");
+
+int[] GetResult(int[] array)
+{
+    int sum = 0;
+    int multiple = 1;
+    var resultArray = new int[2];
+    foreach (var element in array)
+    {
+        if (element > 0)
+        {
+            sum += element;
+        }
+        else
+        {
+            multiple *= element;
+        }
+    }
+    resultArray[0] = sum;
+    resultArray[1] = multiple;
+    return resultArray;
+}
