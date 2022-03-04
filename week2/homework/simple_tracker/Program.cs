98de97946cd1cf97d8a0bef6f276fb0b110d0c93
arya56 <arya56@yahoo.com>
2022-03-04 20:28:52 +0100

    first task

---
+++ week2/homework/simple_tracker/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+var time = new JupiterTime();
+time.Hours = 8;
+time.Minutes = 40;
+PrintTime(time);
+
+
+void PrintTime(JupiterTime jtime)
+{
+    Console.WriteLine($"{jtime.Hours.ToString("D2")}:{jtime.Minutes.ToString("D2")}");
+}
+class JupiterTime
+{
+    public int Hours
+    { get; set; }
+    public int Minutes
+    { get; set; }
+}
