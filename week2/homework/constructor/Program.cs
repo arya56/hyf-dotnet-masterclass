b38cf0c2edd3e1b094dd1810740d57fd9b55c747
arya56 <arya56@yahoo.com>
2022-03-04 21:30:21 +0100

    second task

---
+++ week2/homework/constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+var time = new JupiterTime(14, 88);
+PrintTime(time);
+
+
+void PrintTime(JupiterTime jtime)
+{
+    Console.WriteLine($"{jtime.Hours.ToString("D2")}:{jtime.Minutes.ToString("D2")}");
+}
+class JupiterTime
+{
+    public int Hours
+    { get; set; }
+    public int Minutes
+    { get; set; }
+
+    public JupiterTime(int hours, int minutes)
+    {
+        if (hours > 12)
+        {
+            hours %= 10;
+        }
+        if (minutes > 60)
+        {
+            var extraHours = minutes / 60;
+            minutes %= 60;
+            hours += extraHours;
+
+        }
+        Hours = hours;
+        Minutes = minutes;
+    }
+}
